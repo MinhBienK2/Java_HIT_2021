a7f299376e7d22bfd138a416dfe598c2095477fd
{MinhBienK2} <{phamminhbien333@gmail.com}>
2021-10-13 01:52:30 +0700

    all

---
+++ Buoi1/src/Bai2_HappyNewYears.java
@@ -0,0 +1,23 @@
+public class Bai2_HappyNewYears {
+    public static void main(String[] args) {
+        System.out.println("*       *     *     * * * *    * * * *   *     *");
+        System.out.println("|       |    / \\    |       *  |       *  \\   /");
+        System.out.println("* ----- *   *---*   * * * *    * * * *      *");
+        System.out.println("|       |  /     \\  |          |            |");
+        System.out.println("*       * *       * *          *            *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*     *   ******   *       *       *");
+        System.out.println("| \\   |   |         \\     / \\     /");
+        System.out.println("*  *  *   ******     *   *   *   *");
+        System.out.println("|   \\ |   |           \\ /     \\ /");
+        System.out.println("*     *   ******       *       *       ");
+        System.out.println();
+        System.out.println();
+        System.out.println("*     *   ******      *      * * * *");
+        System.out.println(" \\   /    |          / \\     |       *");
+        System.out.println("   *      ******    *---*    * * * *  ");
+        System.out.println("   |      |        /     \\   |     \\");
+        System.out.println("   *      ******  *       *  *       *");
+    }
+}
