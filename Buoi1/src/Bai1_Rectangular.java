7ad5c73c6c0e56cb6c17a2dabd1f854fd6762504
{MinhBienK2} <{phamminhbien333@gmail.com}>
2021-10-13 09:50:30 +0700

    update

--- Buoi1/src/Bai1_Rectangular.java
+++ Buoi1/src/Bai1_Rectangular.java
@@ -1,10 +1,15 @@
 public class Bai1_Rectangular {
     public static void main(String[] args) {
-        int n=4;
-        int m=4;
+        int n=5;
+        int m=5;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                System.out.print(" * ");
+                if(i==0||i==n-1)
+                    System.out.print(" * ");
+                else if(i!=0 && j==0||i!=n-1 && j==m-1)
+                    System.out.print(" * ");
+                else
+                    System.out.print("   ");
             }
             System.out.println();
         }
